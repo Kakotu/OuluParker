7b2dce7286ed8a4309346eb4629bf8c33d481770
Teemu Karjalainen <t5kate01@students.oamk.fi>
2017-10-10 11:21:08 +0300

    Added colours, icons, styles etc. Locked screen orientation (manifest) Added colours to MapMarkers

--- app/src/main/java/com/kakotu/ouluparker/MapsActivity.java
+++ app/src/main/java/com/kakotu/ouluparker/MapsActivity.java
@@ -3,6 +3,7 @@ package com.kakotu.ouluparker;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
@@ -14,6 +15,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -31,6 +33,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     LatLng oulu = new LatLng(65.0123600, 25.4681600);
 
     private ImageButton buttonParkingPlaces;
+    private ImageButton buttonRefreshPlaces;
 
     Engine engine;
     ArrayList<ParkingPlace> allParkingPlaces;
@@ -45,12 +48,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         buttonParkingPlaces = (ImageButton) findViewById(R.id.buttonParkingPlaces);
+        buttonRefreshPlaces = (ImageButton) findViewById(R.id.buttonRefresh);
 
         buttonParkingPlaces.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent listIntent = new Intent(MapsActivity.this, ParkingSpotListActivity.class);
                 startActivity(listIntent);
+            }
+        });
+
+        buttonRefreshPlaces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMap.clear();
+                new JsonFetchTask().execute();
             }
         });
     }
@@ -78,9 +90,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
         new JsonFetchTask().execute();
-
-        // Add a marker in Oulu and move the camera
-        // mMap.addMarker(new MarkerOptions().position(oulu).title("Marker in Oulu"));
 
     }
 
@@ -120,10 +129,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             LatLng latLng = new LatLng(allParkingPlaces.get(i).getLng(), allParkingPlaces.get(i).getLat());
                             int freeSpaces = allParkingPlaces.get(i).getSpot().getFreeSpace();
 
-                            mMap.addMarker(new MarkerOptions()
+                            MarkerOptions mapMarker = new MarkerOptions()
                                     .position(latLng)
                                     .title(name)
-                                    .snippet("Vapaata: " + freeSpaces));
+                                    .snippet("Vapaata: " + freeSpaces);
+
+                            if (freeSpaces == 0) {
+                                mapMarker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                            } else {
+                                float[] hsv = new float[3];
+                                Color.colorToHSV(getColor(R.color.colorAccent), hsv);
+                                mapMarker.icon(BitmapDescriptorFactory.defaultMarker(hsv[0]));
+                            }
+
+                            mMap.addMarker(mapMarker);
                         }
                     }
 
